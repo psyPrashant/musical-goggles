b640cadd5647e9c71107c715a241326991638cd2
psyHarvey <harvey.bentley@psybergate.co.za>
2026-05-28 12:37:38 +0200

    ci-fix2

--- recruitment-be/src/main/java/com/psybergate/recruitment/RecruitmentApplication.java
+++ recruitment-be/src/main/java/com/psybergate/recruitment/RecruitmentApplication.java
@@ -1,9 +1,12 @@
 package com.psybergate.recruitment;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class RecruitmentApplication {
 
 	public static void main(String[] args) {
